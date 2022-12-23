aeb0701df87e8d5f6784ba2799ccdf24606bf734
Tatiana Pypko <tatiana.pypko@gmail.com>
2022-12-24 00:12:05 +0200

    Added fixes

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const buttonAnswer = document.querySelector('.question');
 const buttonLink = document.querySelector('.link');
 
-buttonAnswer.addEventListener('click', (event) => {
-  let link = prompt('Enter link');
-  if (link) {
-    let isHttp = link.indexOf("http://");
-    let isHttps = link.indexOf("https://");
+buttonAnswer.addEventListener("click", (event) => {
+  const link = document.createElement("input");
+  const inputContainer = document.querySelector(".input-container");
 
-    if (isHttp === 0 || isHttps === 0) {
-      buttonLink.href = link;
-      return;
-    }
-    buttonLink.href = `https://${link}`;
+  if (inputContainer.children.length === 0) {
+    inputContainer.appendChild(link);
   }
+
+  buttonLink.addEventListener("click", (event) => {
+    const { target } = event;
+    target.removeAttribute("href");
+
+    if (link.value) {
+      const linkToLowerCase = link.value.toLowerCase();
+      let isHttp = linkToLowerCase.indexOf("http://");
+      let isHttps = linkToLowerCase.indexOf("https://");
+
+      if (isHttp === 0 || isHttps === 0) {
+        target.href = linkToLowerCase;
+      } else {
+        target.href = `https://${linkToLowerCase}`;
+      }
+
+      link.style.border = "1px solid black";
+      link.value = "";
+    } else {
+      link.style.border = "1px solid red";
+    }
+  });
 });
